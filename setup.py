f605f70d0c861497e881320aff27ed4d02ae94af
Tuomas Virtanen <tuomas.virtanen@qvantel.com>
2017-08-31 15:39:08 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='aiohttp-spyne',
-    version='0.1.0',
+    version='0.1.1',
     description='Aiohttp transport for Spyne RPC library',
     long_description=long_description,
     url='https://github.com/katajakasa/aiohttp-spyne',
